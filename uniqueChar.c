9414ee8209c9ea2546a99fad7b736d5c310baa34
Suryakalai <suryakalai96@gmail.com>
2017-06-20 13:11:55 +0100

    Create uniqueChar.c

---
+++ uniqueChar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char a[]="malayalam";
+	int i,j;
+	for(i=0;i<strlen(a);i++){
+		for(j=i+1;j<strlen(a);j++){
+			if(a[i]==a[j]){
+				a[j]='0';
+			}	
+		}
+		if(a[i]!='0'){
+			printf("%c",a[i]);
+		}
+	}
+	printf("\n");
+}
